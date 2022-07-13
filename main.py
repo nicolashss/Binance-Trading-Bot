bc0ddf1352a04dd77d05a5f9bf8b7a2b40e1cd4d
nicolas.shs <contact.nicolas.shs@gmail.com>
2022-07-13 15:43:55 +0200

    Imports
    V.1.0.1

---
+++ main.py
@@ -0,0 +1,44 @@
+# -------------------------
+
+# Author: Doom & LuX
+# Github : https://github.com/nicolashss/TradingBOT
+# Website : https://xlux.xyz/
+
+# -------------------------
+
+# Imports
+# ---------
+
+import os 
+import sys
+import time
+import os
+import sys
+
+from colorama import init, Fore, Back, Style
+from pprint import pprint
+
+from time import sleep, time
+
+from binance.client import Client
+from binance.exceptions import BinanceAPIException
+
+import pandas_ta as ta
+
+import json
+import math
+import time
+
+import telepot
+
+import pandas as pd     # needs pip install
+import numpy as np
+import matplotlib.pyplot as plt   # needs pip install
+from operator import add
+from operator import sub
+
+from datetime import datetime
+
+import yaml
+
+# ---------
